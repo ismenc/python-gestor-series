3f98dc9db4f895702adb243891a7afd275638506
diego <diego>
2018-02-16 11:14:06 +0100

    leer fichero por lineas

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+from formatter import NullFormatter
+
+archivo = open("Usuarios.txt", "a+")
+
+nombre_usuario = raw_input("Introduce tu nombre de usuario: ")
+password = raw_input("Introduce tu contraseña: ")
+
+datos_usuario = nombre_usuario+ ", "+password
+
+linea = archivo.readline()
+while linea != "fin":
+    print linea
+    linea = archivo.readline()
+    #usar metodo linea.find() para encontrar la posicion de la , y partir la cadena
+
+#archivo.write("\n"+datos_usuario)
