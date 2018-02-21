bc075be5718b4e4911212d1c534fa78fa280a283
Edmuv <eduardobasket6@gmail.com>
2018-02-21 20:04:19 +0100

    un poco de la clase Usuario, tiene errores

---
+++ src/modelo/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import Usuario as u
+
+usuario = u.Usuario("Edu", "qwe", 22)
+print (usuario.nombre)
+print (usuario.edad)
+
+visto = ["007", "300", "Harry Potter"]
+print ("Las peliculas son: " + ", ".join(visto))
