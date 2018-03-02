e3815e605ca00cf51a102ce86f7cc99eec3a0a78
Ismael Núñez <ismael.inc@gmail.com>
2018-03-02 16:09:52 +0100

    Correccion ficheros

---
+++ src/datos/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+''' Clase dónde probaremos cosas de este paquete '''
---
+++ src/modelo/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+''' Clase dónde probaremos cosas de este paquete 
+
+import Pelicula as p
+import Usuario as u
+
+usuario = u.Usuario("Edu", "qwe", 22)
+print (usuario.nombre)
+print (usuario.edad)
+
+peli = p.Pelicula("Star wars", 1980, "Sci-fi", "Lucas noseke", 108)
+peli2 = p.Pelicula("Monsters SA", 2005, "Infantil", "Uno de 2hermana", 77)
+peli3 = p.Pelicula("Diarrea", 2018, "fantasía", "yo", 9999999)
+
+usuario.addAVisto(peli.titulo)
+usuario.addAVisto(peli2.titulo)
+usuario.addAVisto(peli.titulo)
+
+
+visto = [peli.titulo, peli2.titulo, peli3.titulo]
+print ("Las peliculas son: " + ", ".join(usuario.visto))
+
+
+'''
